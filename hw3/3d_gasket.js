9cca594a9112300721b1982c7f7ec8c727374dad
Anli Ji <anli.azuresand@gmail.com>
2014-09-15 18:18:04 -0400

    Remove unnecessary comment lines

--- hw3/3d_gasket.js
+++ hw3/3d_gasket.js
@@ -1,10 +1,10 @@
 // Computer Graphics Homework 3 by Anli Ji, Xitu Chen
 
 var baseColors = [
-    vec4(1.0, 0.0, 0.0),
-    vec4(0.0, 1.0, 0.0),
-    vec4(0.0, 0.0, 1.0),
-    vec4(1.0, 1.0, 1.0)
+    vec4(1.0, 1.0, 0.0,1),
+    vec4(0.0, 1.0, 1.0,1),
+    vec4(0.0, 0.0, 1.0,1),
+    vec4(1.0, 1.0, 0,1)
 ];
 
 var colors = [];
@@ -20,11 +20,7 @@ function tentrahedronToTrangle(tentrahedrons){
         verts.push([tentrahedrons[i][0],tentrahedrons[i][2],tentrahedrons[i][3]]);
         verts.push([tentrahedrons[i][1],tentrahedrons[i][2],tentrahedrons[i][3]]);
         for (var j=0; j<4; j++) {
-            var temp = [];
-            for (var k=0; k<3; k++) {
-                temp.push(baseColors[j]);
-            }
-            colors.push(temp);
+            colors.push([baseColors[j],baseColors[j],baseColors[j]]);
         }
     }
     return verts;
@@ -32,6 +28,7 @@ function tentrahedronToTrangle(tentrahedrons){
 
 // draw the verts that would eventually forms the final shape
 function draw(verts){
+    console.log(colors);
     var canvas = document.getElementById( "gl-canvas" );
     
     gl = WebGLUtils.setupWebGL( canvas );
@@ -40,7 +37,7 @@ function draw(verts){
     //  Configure WebGL
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.enable(gl.DEPTH_TEST);
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
     
     //  Load shaders and initialize attribute buffers
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
@@ -105,69 +102,7 @@ window.onload = function init()
     var v4 = vec3(0,0.6, 0.2);
     verts = [[v1,v2,v3,v4]];
 
-    draw(tentrahedronToTrangle(splitting(splitting(splitting(verts)))));
-
-    // draw(tentrahedronToTrangle(splitting(verts));
-
-    // var verts = [[v1,v2,v3]];
-    // var points = [];
-
-    // for(var i=0;i<10000;i++){
-    //     var randomPoint = Math.floor((Math.random() * 3) + 1);
-    //     if (randomPoint == 1){
-    //         q = vec3((v1[0]+p[0])/2,(v1[1]+p[1])/2,(v1[2]+p[2])/2)
-    //     }
-    //     else if (randomPoint == 2){
-    //         q = vec3((v2[0]+p[0])/2,(v2[1]+p[1])/2,(v2[2]+p[2])/2)
-    //     }
-    //     else{
-    //         q = vec3((v3[0]+p[0])/2,(v3[1]+p[1])/2,(v3[2]+p[2])/2)
-    //     }
-    //     points.push([q]);
-    //     p = q;
-    // }
-
-    // //
-    // //  Configure WebGL
-    // //
-    // gl.viewport( 0, 0, canvas.width, canvas.height );
-    // gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
-    
-    // //  Load shaders and initialize attribute buffers
-    // var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    // gl.useProgram( program );
-    
-    // gl.clear( gl.COLOR_BUFFER_BIT );
-    
-    // for (var i=0; i<verts.length; i++) {
-    //     // Load the data into the GPU
-    //     var bufferId = gl.createBuffer();
-    //     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    //     gl.bufferData( gl.ARRAY_BUFFER, flatten(verts[i]), gl.STATIC_DRAW );
-
-    //     // Associate out shader variables with our data buffer
-
-    //     var vPosition = gl.getAttribLocation( program, "vPosition" );
-    //     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
-    //     gl.enableVertexAttribArray( vPosition );
-        
-    //     gl.drawArrays( gl.LINE_LOOP, 0, verts[i].length );
-    // }
-
-    // for (var i=0; i<points.length; i++) {
-    //     // Load the data into the GPU
-    //     var bufferId = gl.createBuffer();
-    //     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    //     gl.bufferData( gl.ARRAY_BUFFER, flatten(points[i]), gl.STATIC_DRAW );
-
-    //     // Associate out shader variables with our data buffer
-
-    //     var vPosition = gl.getAttribLocation( program, "vPosition" );
-    //     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
-    //     gl.enableVertexAttribArray( vPosition );
-        
-    //     gl.drawArrays( gl.POINTS, 0 ,points[i].length);
-    // }
+    draw(tentrahedronToTrangle(splitting(splitting(splitting(splitting(splitting(verts)))))));
 };
 
 
