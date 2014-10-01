6bc4cd02340cf8ac6d40f0d2a25c9ffc868344b6
Anli Ji <anli.azuresand@gmail.com>
2014-09-30 23:00:38 -0400

    Code complete for hw5

--- hw5/cad.js
+++ hw5/cad.js
@@ -5,23 +5,37 @@ var gl;
 var maxNumVertices  = 200;
 var index = 0;
 
-var cindex = 0;
+// var cindex = 0;
 
-var colors = [
+var componentR = 0.0
+var componentG = 0.0
+var componentB = 0.0
+var sliderColor = vec4(0.0,0.0,0.0,1.0)
 
-    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
-    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
-    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
-    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
-    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
-    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
-    vec4( 0.0, 1.0, 1.0, 1.0)   // cyan
-];    
+// var colors = [
+
+//     vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+//     vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+//     vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+//     vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+//     vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+//     vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+//     vec4( 0.0, 1.0, 1.0, 1.0)   // cyan
+// ];    
 var t;
 var numPolygons = 0;
 var numIndices = [];
 numIndices[0] = 0;
 var start = [0];
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
 
 window.onload = function init() {
     canvas = document.getElementById( "gl-canvas" );
@@ -29,18 +43,43 @@ window.onload = function init() {
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
     
-    var m = document.getElementById("mymenu");
+    // var m = document.getElementById("mymenu");
     
-    m.addEventListener("click", function() {
-       cindex = m.selectedIndex;
-        });
+    // m.addEventListener("click", function() {
+    //    cindex = m.selectedIndex;
+    //     });
+
+    document.getElementById("slideR").onchange =
+        function() {
+            componentR = parseInt(document.getElementById("slideR").value)
+            sliderColor = vec4(componentR/255,componentG/255,componentB/255,1.0)
+            console.log(sliderColor)
+            document.getElementById("colorSample").style.backgroundColor = rgbToHex(componentR,componentG,componentB)            
+        }
+
+    document.getElementById("slideG").onchange =
+        function() {
+            componentG = parseInt(document.getElementById("slideG").value)
+            sliderColor = vec4(componentR/255,componentG/255,componentB/255,1.0)
+            console.log(sliderColor)
+            document.getElementById("colorSample").style.backgroundColor = rgbToHex(componentR,componentG,componentB)
+
+        }
+
+    document.getElementById("slideB").onchange =
+        function() {
+            componentB = parseInt(document.getElementById("slideB").value)
+            sliderColor = vec4(componentR/255,componentG/255,componentB/255,1.0)
+            console.log(sliderColor)
+            document.getElementById("colorSample").style.backgroundColor = rgbToHex(componentR,componentG,componentB)
+        }
         
     var a = document.getElementById("Button1")
     a.addEventListener("click", function(){
-    numPolygons++;
-    numIndices[numPolygons] = 0;
-    start[numPolygons] = index;
-    render();
+        numPolygons++;
+        numIndices[numPolygons] = 0;
+        start[numPolygons] = index;
+        render();
     });
 
     canvas.addEventListener("mousedown", function(){
@@ -49,7 +88,7 @@ window.onload = function init() {
         gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
         gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(t));
 
-        t = vec4(colors[cindex]);
+        t = vec4(sliderColor);
         
         gl.bindBuffer( gl.ARRAY_BUFFER, cBufferId );
         gl.bufferSubData(gl.ARRAY_BUFFER, 16*index, flatten(t));
